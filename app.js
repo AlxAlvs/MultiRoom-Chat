255836302aeec567b40d7bb2fc71c9b299473581
Alex Alves <AlxAlvsContato@gmail.com>
2020-01-27 16:31:27 -0300

    adiciona configuracao para envio de mensagens

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
-/*parametrizar as portas de escuta*/
-app.listen(80, function(){
+/* parametrizar porta de escuta */
+var server = app.listen(80, function(){
     console.log('Servidor online');
 })
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* Criar a conexão por WebSocket */
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    });
+
+    socket.on('msgParaServidor', function(data){
+        /* eventos de dialogo*/
+        socket.emit(
+            'msgParaCliente',
+            {apelido: data.apelido, mensagem: data.mensagem});
+    
+        socket.broadcast.emit(
+            'msgParaCliente',
+            {apelido: data.apelido, mensagem: data.mensagem});
+        
+
+        /*participantes*/
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente',
+                {apelido: data.apelido}
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                {apelido: data.apelido}
+            );
+         }
+    });
+});
